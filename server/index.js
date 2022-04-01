1367446d5cd8892dfc8c6b0bd8b4a4b45be76086
Andrew Cring <amcring1@gmail.com>
2022-03-31 17:34:44 -0700

    Following the directions for the exercise

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
-const express = require('express')
+const express = require('express');
+const cors = require("cors");
 
 const app = express()
 
-app.use(express.json()) // When we want to be able to accept JSON.
+app.use(express.json()); // When we want to be able to accept JSON.
+app.use(cors()); //When we want to be able to accept cors
 
 app.listen(4000, () => console.log('Server running on 4000'))
